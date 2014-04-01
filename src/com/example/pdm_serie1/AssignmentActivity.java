7a28f999a8cb245f3f156f1c4756892c475e725c
PPedroso <pedroso_pedro@hotmail.com>
2014-04-01 16:39:06 +0100

    Hotfix

--- src/com/example/pdm_serie1/AssignmentActivity.java
+++ src/com/example/pdm_serie1/AssignmentActivity.java
@@ -67,7 +67,6 @@ public class AssignmentActivity extends Activity {
 					
 					for(int i=0;i<a.length();++i){
 						resultList.add(((JSONObject)a.get(i)).get("title").toString());
-						++i;
 					}
 					
 					return resultList;
